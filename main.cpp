d5b5cf5716240123fd4e409b4fcd4bd87b2db9b1
meriadec <meriadec.pillet@gmail.com>
2015-02-12 18:29:41 +0100

    feat(core): add test in main

--- main.cpp
+++ main.cpp
@@ -11,12 +11,31 @@
 
 #include <iostream>
 #include "Vm.hpp"
+#include "avm.hpp"
+
+Vm * ft_getVm (void)
+{
+	static Vm * out = NULL;
+
+	if (!out) {
+		out = new Vm();
+	}
+
+	return out;
+}
 
 int main (void)
 {
 	Vm vm;
 
-	IOperand const * test = vm.createOperand(Int8, "30");
-	std::cout << test->toString() << std::endl;
+	IOperand const * test  = vm.createOperand(Int8, "20");
+	IOperand const * test2 = vm.createOperand(Int16, "22");
+	IOperand const * res   = (*test) + (*test2);
+
+	std::cout << "ce que l'on veut:" << std::endl;
+	std::cout << "42" << std::endl;
+	std::cout << "ce que l'on a   :" << std::endl;
+	std::cout << res << std::endl;
+
 	return (0);
 }
